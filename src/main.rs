bbe7a6111e37e316027c0a13444d4d954a7c3036
Sai Yashwanth <taddishetty34@gmail.com>
2023-12-22 22:35:30 +0530

    readme

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 
 fn usage(){
+
+    // let ver: u32;
+    let ver = get_version();
     println!("mdcompiler, A tiny markdown compiler written in rust by Sai Yashwanth");
-    println!("Version {}", get_version());
+    println!("Version {}", ver);
 }
 
 fn get_version() -> u32{
-    1000
+    100
 }
 
 fn main() {

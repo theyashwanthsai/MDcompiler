c110d5a5ad72cf4dcc41dd320ea9dfc32f2a66e9
Sai Yashwanth <taddishetty34@gmail.com>
2023-12-22 22:30:25 +0530

    init

--- target/debug/deps/mdcompiler-a0389b49ea85a980.d
+++
@@ -1,5 +0,0 @@
-/Users/saiyashwanth/Dev/Markdown compiler/Markdown-compiler-in-rust/target/debug/deps/mdcompiler-a0389b49ea85a980: src/main.rs
-
-/Users/saiyashwanth/Dev/Markdown compiler/Markdown-compiler-in-rust/target/debug/deps/mdcompiler-a0389b49ea85a980.d: src/main.rs
-
-src/main.rs:
--- target/debug/mdcompiler.d
+++
@@ -1,1 +0,0 @@
-/Users/saiyashwanth/Dev/Markdown\ compiler/Markdown-compiler-in-rust/target/debug/mdcompiler: /Users/saiyashwanth/Dev/Markdown\ compiler/Markdown-compiler-in-rust/src/main.rs

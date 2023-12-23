7c218d483216182419b0d3a8266d05ecac6f3cfa
Sai Yashwanth <taddishetty34@gmail.com>
2023-12-23 16:44:49 +0530

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
+fn parse_md(){
+
+}
+
+
+
+fn print_long_banner(){
+    print_short_banner();
+    println!("Written by: {}", env!("CARGO_PKG_AUTHORS"));
+    println!("Usage: mdcompiler <somefile>.md");
+}
+
+
+fn print_short_banner(){
+    println!("{}", get_title());
+}
+
+
+fn get_title() -> String{
+    let mut title = String::from(env!("CARGO_PKG_NAME"));
+    title.push_str(" (v");
+    title.push_str(env!("CARGO_PKG_VERSION"));
+    title.push_str("), ");
+    title.push_str(env!("CARGO_PKG_DESCRIPTION"));
+    title
+}
 
 fn usage(){
-
-    // let ver: u32;
-    // let ver = get_version();
-    let ver = "0.1.0";
-    println!("mdcompiler, A tiny markdown compiler written in rust by Sai Yashwanth");
-    println!("Version {}", ver);
+    print_long_banner();
 }
 
-fn get_version() -> u32{
-    100
-}
-
-fn main() {
-    // println!("Hello, world!");
+fn main(){
     usage();
-    
 }

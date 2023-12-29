73ae8ac848c5ee99d85ad14cb53394e5ab00d743
Sai Yashwanth <taddishetty34@gmail.com>
2023-12-29 23:19:36 +0530

    finished parser. First order and p tag

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use::std::io::{BufRead, BufReader};
 use::std::fs::File;
 use::std::path::Path;
+use::std::io::Write;
 
-fn parse_md(filename: &str){
+fn parse_md(_filename: &str){
     print_short_banner();
-    println!("[ INFO ] Trying to parse {}....", filename);
+    println!("[ INFO ] Trying to parse {}....", _filename);
     
-    let input_filename = Path::new(filename);
+    let input_filename = Path::new(_filename);
 
     let file = File::open(&input_filename).expect("[ ERROR ] Failed to open file");
 
@@ -21,11 +22,69 @@ fn parse_md(filename: &str){
     for line in reader.lines(){
         let line_content = line.unwrap();
         // println!("{}", line_content);
-        
+        let mut first_char: Vec<char> = line_content.chars().take(1).collect();
+        let mut output_line = String::new();
+
+
+        match first_char.pop() {
+            Some('#') => {
+                // First order heading (h1)
+                if pflag{
+                    pflag = false;
+                    output_line.push_str("</p>\n");
+                }
+                if h1flag{
+                    h1flag = false;
+                    output_line.push_str("</h1>\n");
+                }
+
+                h1flag = true;
+                output_line.push_str("\n\n<h1>");
+                output_line.push_str(&line_content[2..]);
+            }
+            _ => {
+                // not #, could be anything
+                if !pflag{
+                    pflag = true;
+                    output_line.push_str("<p>");
+                }
+                output_line.push_str(&line_content);
+            }
+        }
+
+        if pflag{
+            pflag = false;
+            output_line.push_str("</p>\n");
+        }
+
+        if h1flag{
+            h1flag = false;
+            output_line.push_str("</h1>\n");
+        }
+
+        if output_line != "<p></p>\n"{
+            tokens.push(output_line);
+        }
         
     }
 
-    
+    // Debug for testing
+    // for t in tokens{
+    //     println!("{}", t);
+    // }
+
+    let mut output_filename  = String::from(&_filename[.._filename.len()-3]);
+    output_filename.push_str(".html");
+
+    let mut output_file = File::create(output_filename).expect("[ ERROR ] Could not create html!");
+
+    for line in tokens{
+        output_file.write_all(line.as_bytes()).expect("[ ERROR ] Could not write output file!");
+    }
+
+    println!("[ INFO ] Parsing complete!");
+
+
 }
 
 
